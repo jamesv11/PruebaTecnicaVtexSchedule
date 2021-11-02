ed823b22025120cdbea7480c4db77252a47e395a
james <jamesvans1234@gmail.com>
2021-11-02 13:42:16 -0500

    Home page termiandat

--- PruebaTecnicaVtex/ClientApp/src/pages/HomePage.tsx
+++ PruebaTecnicaVtex/ClientApp/src/pages/HomePage.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const HomePage = () => {
-  return <h2>Home Page</h2>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        backgroundColor: "#2f74c0",
+        fontFamily: "Neucha, cursive",
+        height: "100%",
+        minHeight: "100vh",
+      }}
+    >
+      <h1 className={"title_item"} style={{ color: "#23A4CA" }}>
+        Tu <span style={{ color: "#30C1EC" }}>agenda </span>
+      </h1>
+      <h3 style={{ borderBottom: "1px solid #000" }}>Haz tu propia agenda!</h3>
+    </div>
+  );
 };
 
 export default HomePage;
